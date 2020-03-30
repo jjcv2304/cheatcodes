75b58594484ee1f37c8d8dc8534656937e3eeb06
Juan <jjcv2304@hotmail.com>
2020-03-30 09:13:36 -0700

    Upadting angular 8 to 9 . Step 1

--- CheatCodes.UI/src/app/app.module.ts
+++ CheatCodes.UI/src/app/app.module.ts
@@ -1,24 +1,24 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { HttpClientModule } from "@angular/common/http";
-import { FlipModule } from "ngx-flip";
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+import {FlipModule} from 'ngx-flip';
 
-import { AppComponent } from "./app.component";
-import { CategoriesListComponent } from "./categories-generation/categories-list/categories-list.component";
-import { CategoriesService } from "./categories-generation/categories/categories.service";
-import { MainMenuComponent } from "./main-menu/main-menu.component";
-import { CategoryCardComponent } from "./categories-generation/category-card/category-card.component";
-import { CategoryEditComponent } from "./categories-generation/category-edit/category-edit.component";
-import { AppRoutingModule } from "./app-routing.module";
-import { FormsModule } from "@angular/forms";
-import { FieldEditComponent } from "./categories-generation/field-edit/field-edit.component";
-import { CardMoveMenuComponent } from "./categories-generation/category-card/card-move-menu/card-move-menu.component";
-import { CategoriesSearchModule } from "./categories-search/categories-search.module";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { MatSliderModule } from "@angular/material/slider";
-import { CategoriesContainerComponent } from
-  "./categories-generation/categories-container/categories-container.component";
-import { MatProgressSpinnerModule } from "@angular/material";
+import {AppComponent} from './app.component';
+import {CategoriesListComponent} from './categories-generation/categories-list/categories-list.component';
+import {CategoriesService} from './categories-generation/categories/categories.service';
+import {MainMenuComponent} from './main-menu/main-menu.component';
+import {CategoryCardComponent} from './categories-generation/category-card/category-card.component';
+import {CategoryEditComponent} from './categories-generation/category-edit/category-edit.component';
+import {AppRoutingModule} from './app-routing.module';
+import {FormsModule} from '@angular/forms';
+import {FieldEditComponent} from './categories-generation/field-edit/field-edit.component';
+import {CardMoveMenuComponent} from './categories-generation/category-card/card-move-menu/card-move-menu.component';
+import {CategoriesSearchModule} from './categories-search/categories-search.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatSliderModule} from '@angular/material/slider';
+import {CategoriesContainerComponent} from
+    './categories-generation/categories-container/categories-container.component';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 
 @NgModule({
