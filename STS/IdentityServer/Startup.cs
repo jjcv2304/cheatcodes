ed7e678d9545d6db5a83cccbf2fcb3c6ab2ba38a
Juan <jjcv2304@hotmail.com>
2020-04-01 17:24:50 -0700

    Moving IdS4 Configuration from in memory to Db

--- STS/IdentityServer/Startup.cs
+++ STS/IdentityServer/Startup.cs
@@ -2,20 +2,27 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using System;
+using System.Reflection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using IdentityServer4.Configuration;
+using Microsoft.Extensions.Configuration;
 
 namespace IdentityServer
 {
   public class Startup
   {
     public IWebHostEnvironment Environment { get; }
+    public IConfiguration Configuration { get; }
 
-    public Startup(IWebHostEnvironment environment)
+    public Startup(IWebHostEnvironment environment, IConfiguration configuration)
     {
       Environment = environment;
+      Configuration = configuration;
     }
 
     public void ConfigureServices(IServiceCollection services)
@@ -23,10 +30,33 @@ namespace IdentityServer
       // uncomment, if you want to add an MVC-based UI
       services.AddControllersWithViews();
 
-      var builder = services.AddIdentityServer()
-          .AddInMemoryIdentityResources(Config.Ids)
-          .AddInMemoryApiResources(Config.Apis)
-          .AddInMemoryClients(Config.Clients);
+      string connectionString = Configuration.GetConnectionString("DefaultConnection");
+
+      var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+
+      var builder = services.AddIdentityServer(options =>
+        {
+          options.Events.RaiseErrorEvents = true;
+          options.Events.RaiseInformationEvents = true;
+          options.Events.RaiseFailureEvents = true;
+          options.Events.RaiseSuccessEvents = true;
+          options.UserInteraction.LoginUrl = "/Account/Login";
+          options.UserInteraction.LogoutUrl = "/Account/Logout";
+          options.Authentication = new AuthenticationOptions()
+          {
+            CookieLifetime = TimeSpan.FromHours(10), // ID server cookie timeout set to 10 hours
+            CookieSlidingExpiration = true
+          };
+        })
+        .AddConfigurationStore(options =>
+        {
+          options.ConfigureDbContext = b => b.UseSqlServer(connectionString, sql => sql.MigrationsAssembly(migrationsAssembly));
+        })
+        .AddOperationalStore(options =>
+        {
+          options.ConfigureDbContext = b => b.UseSqlServer(connectionString, sql => sql.MigrationsAssembly(migrationsAssembly));
+          options.EnableTokenCleanup = true;
+        });
 
       // not recommended for production - you need to store your key material somewhere secure
       builder.AddDeveloperSigningCredential();
