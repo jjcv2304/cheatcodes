fa8c58f584de60f4dd444f01d10363ef213cf14c
Juan <jjcv2304@hotmail.com>
2020-04-02 12:54:48 -0700

    Another try adding IdentityServer and Asp Identity, with custom views, like Register

---
+++ STS/IdSNet/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using IdSNet.Data;
+using IdSNet.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(IdSNet.Areas.Identity.IdentityHostingStartup))]
+namespace IdSNet.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ STS/IdSNet/Config.cs
@@ -0,0 +1,54 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace IdSNet
+{
+    public static class Config
+    {
+      public static IEnumerable<IdentityResource> Ids =>
+        new IdentityResource[]
+        {
+          new IdentityResources.OpenId(),
+          new IdentityResources.Profile(),
+        };
+
+
+      public static IEnumerable<ApiResource> Apis =>
+        new ApiResource[]
+        {
+          new ApiResource("mainApp-api", "My API #1")
+        };
+
+
+      public static IEnumerable<Client> Clients =>
+        new Client[]
+        {
+          // SPA client using code flow + pkce
+          new Client
+          {
+            ClientId = "mainApp-api",
+            ClientName = "SPA Client",
+            ClientUri = "http://identityserver.io",
+
+            AllowedGrantTypes = GrantTypes.Code,
+            RequirePkce = true,
+            RequireClientSecret = false,
+
+            RedirectUris = {"http://localhost:4200/signin-callback", 
+              "http://localhost:4200/assets/silent-callback.html"},
+
+            PostLogoutRedirectUris = {"http://localhost:4200/signout-callback" },
+            AllowedCorsOrigins = { "http://localhost:4200" },
+
+            AllowedScopes = { "openid", "profile", "mainApp-api" },
+            AccessTokenLifetime = 600
+
+          }
+                
+        };
+    }
+}
