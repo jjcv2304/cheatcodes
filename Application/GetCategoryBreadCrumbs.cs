e37f0b4fdff486a40433319294905932c06b2426
Juan <jjcv2304@hotmail.com>
2020-05-27 07:45:53 -0700

    BreadCrumbs  Query

---
+++ Application/GetCategoryBreadCrumbs.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.Utils;
+using Application.Utils.Interfaces;
+using Dtos;
+
+namespace Application
+{
+  public sealed class GetCategoryBreadCrumbs : IQuery<CategoryBreadCrumbsDto>
+  {
+    public int RootCategoryId { get; }
+
+    public GetCategoryBreadCrumbs(int rootCategoryId)
+    {
+      RootCategoryId = rootCategoryId;
+    }
+
+    public sealed class GetCategoryBreadCrumbsQueryHandler : IQueryHandler<GetCategoryBreadCrumbs, CategoryBreadCrumbsDto>
+    {
+      private readonly ICategoryQueryRepository _categoryQueryRepository;
+
+      public GetCategoryBreadCrumbsQueryHandler(ICategoryQueryRepository categoryQueryRepository)
+      {
+        _categoryQueryRepository = categoryQueryRepository;
+      }
+
+      public CategoryBreadCrumbsDto Handle(GetCategoryBreadCrumbs query)
+      {
+        var categoryRoot = _categoryQueryRepository.ParentsCategoryTreeDtos(query.RootCategoryId);
+        CategoryBreadCrumbsDto categoryBreadCrumbsDto = MapService.Map(categoryRoot);
+        return categoryBreadCrumbsDto;
+      }
+    }
+  }
+}
