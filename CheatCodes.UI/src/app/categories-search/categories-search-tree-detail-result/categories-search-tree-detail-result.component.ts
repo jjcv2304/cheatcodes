2ba1c259c4a3cdce1b01203f05c698db0c64c36b
Juan <jjcv2304@hotmail.com>
2020-06-04 13:37:31 -0700

    Filter angular tree

--- CheatCodes.UI/src/app/categories-search/categories-search-tree-detail-result/categories-search-tree-detail-result.component.ts
+++ CheatCodes.UI/src/app/categories-search/categories-search-tree-detail-result/categories-search-tree-detail-result.component.ts
@@ -3,7 +3,8 @@ import {Component, Input, OnChanges, OnInit} from '@angular/core';
 import {CategoryTree} from '../model/category';
 import {FlatTreeControl} from '@angular/cdk/tree';
 import {MatTreeFlatDataSource, MatTreeFlattener, MatTreeNode, MatTreeNodeDef, MatTree} from '@angular/material/tree';
-import {BehaviorSubject, Observable} from 'rxjs';
+import {BehaviorSubject, Observable, Subject} from 'rxjs';
+import {debounceTime, distinctUntilChanged} from 'rxjs/operators';
 
 interface FlatNode {
   expandable: boolean;
@@ -11,6 +12,7 @@ interface FlatNode {
   description: string;
   parentId: number;
   level: number;
+  isVisible: boolean;
 }
 
 @Component({
@@ -18,11 +20,12 @@ interface FlatNode {
   templateUrl: './categories-search-tree-detail-result.component.html',
   styleUrls: ['./categories-search-tree-detail-result.component.scss']
 })
-export class CategoriesSearchTreeDetailResultComponent implements OnChanges {
+export class CategoriesSearchTreeDetailResultComponent implements OnChanges, OnInit {
 
   @Input() cardDetail: CategoryTree;
   selectedCard: CategoryTree;
   treeWidth = '25%';
+  searchFilter: Subject<string> = new Subject<string>();
 
   private _transformer = (node: CategoryTree, level: number) => {
     return {
@@ -31,8 +34,9 @@ export class CategoriesSearchTreeDetailResultComponent implements OnChanges {
       description: node.description,
       parentId: node.parentId,
       level: level,
+      isVisible: true
     };
-  }
+  };
 
   treeControl = new FlatTreeControl<FlatNode>(node => node.level, node => node.expandable);
 
@@ -46,6 +50,17 @@ export class CategoriesSearchTreeDetailResultComponent implements OnChanges {
 
   hasChild = (_: number, node: FlatNode) => node.expandable;
   activeNode: CategoryTree;
+
+  ngOnInit(): void {
+    this.searchFilter.pipe(debounceTime(500), distinctUntilChanged())
+      .subscribe(value => {
+        if (value && value.length >= 3) {
+          this.filterByContent(value);
+        } else {
+          this.clearFilter();
+        }
+      });
+  }
 
   ngOnChanges() {
     let cardsDetails: [CategoryTree];
@@ -69,5 +84,43 @@ export class CategoriesSearchTreeDetailResultComponent implements OnChanges {
       this.treeWidth = '25%';
     }
 
+    // if (this.treeWidth === '25%') {
+    //   this.treeWidth = '35%';
+    //   this.filterByName('in');
+    // } else if (this.treeWidth === '35%') {
+    //   this.treeWidth = '45%';
+    //   this.filterByName('es');
+    // } else if (this.treeWidth === '45%') {
+    //   this.treeWidth = '25%';
+    //   this.clearFilter();
+    // }
+
   }
+
+  private filterByContent(term: string): void {
+    const filteredItems = this.treeControl.dataNodes.filter(
+      x => ((x.name.toLowerCase().indexOf(term.toLowerCase()) === -1) && (x.description.toLowerCase().indexOf(term.toLowerCase()) === -1))
+    );
+    filteredItems.map(x => {
+      x.isVisible = false;
+    });
+
+    const visibleItems = this.treeControl.dataNodes.filter(
+      x => ((x.name.toLowerCase().indexOf(term.toLowerCase()) > -1) || (x.description.toLowerCase().indexOf(term.toLowerCase()) > -1))
+    );
+    visibleItems.map(x => {
+      x.isVisible = true;
+    });
+    this.treeControl.expandAll();
+  }
+
+  private clearFilter(): void {
+    this.treeControl.dataNodes.forEach(x => x.isVisible = true);
+    this.treeControl.collapseAll();
+  }
+
+  filterByName(filter: string): void {
+    this.searchFilter.next(filter);
+  }
+
 }
