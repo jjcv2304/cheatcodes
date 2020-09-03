59b075d1c83ddd9105604a1ae11c33d509c6d317
Juan <jjcv2304@hotmail.com>
2020-09-03 13:19:46 -0700

    Adding Selenium Tests

---
+++ Api.Test/Framework/ChromeDriverWithLoginFixture.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace Api.Test.Framework
+{
+  public sealed class ChromeDriverWithLoginFixture : IDisposable
+  {
+    public IWebDriver Driver { get; private set; }
+
+    public ChromeDriverWithLoginFixture()
+    {
+      Driver = new ChromeDriver();
+      Login();
+    }
+
+    private void Login()
+    {
+
+      Driver.Navigate().GoToUrl(SeleniumConfig.LoginUrl);
+      Driver.FindElement(By.Id("Username")).SendKeys("h@h.com");
+      Driver.FindElement(By.Id("Password")).SendKeys("Test123$");
+      Driver.FindElement(By.ClassName("btn-primary")).Click();
+      Driver.Navigate().GoToUrl(SeleniumConfig.CardsUrl);//to force the first redirect
+      WebDriverWait wait = new WebDriverWait(Driver, timeout: TimeSpan.FromSeconds(11));
+       wait.Until(ExpectedConditions.ElementIsVisible(By.TagName("app-categories-list")));
+    }
+
+    public void Dispose()
+    {
+      Driver.Dispose();
+    }
+  }
+
+}
